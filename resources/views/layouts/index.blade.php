3a9e04f47f4178aa2667909fdcce1e5dbb3f5f8c
FrancineHuang <huangxin130@gmail.com>
2023-02-16 23:20:10 +0900

    app.blade.phpのテンプレートを差し替える予定。

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>食華 - 本場の中華料理を探そう</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="中国人が作る本場中華料理のレシピシェアサービス。本場中華を作りたいならココ！作り方検索できる。本場中華料理が大好きな人は自分のレシピを作成し、公開できる。">
+    <meta name="keywords" content="食華,Shokuka,しょくか,本場中華,中華料理,中国料理,中華レシピ,レシピサービス,ChineseFood,Recipe">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <!--Headerはここから-->
+    <header>
+        <div class="flex bg-red-800">
+            <h1 class="text-neutral-50 flex-1 text-lg mt-4 ml-5">
+                <a href="index.html">Shokuka</a>
+            </h1>
+            <div class="flex-grow-0 mb-3 mt-3">
+              <a class="mr-2 inline-block px-4 py-3 text-xs text-neutral-50 hover:text-red-200 leading-none border border-neutral-50 hover:border-neutral-50 rounded" href="#">新規登録</a>
+              <a class="inline-block px-4 py-3 text-xs text-neutral-50 hover:text-red-200 leading-none border border-neutral-50 hover:border-neutral-50 rounded" href="#">ログイン</a>
+            </div>
+            </ul>
+        </div>
+    </header>
+    <!--Headerはここまで-->
+
+    <main>
+    <!--Mainvisualはここから-->
+    <section class="bg-gray-200">
+        <div class="container mx-auto py-40 relative">
+          <h1 class="mt-2 text-4xl font-bold font-heading text-center">Share Your Chinese Cooking Recipes</h1>
+          <p class="text-center pt-5">たまに刺激的な料理を食べたくなる<br>
+            たまに本場の中華料理を作りたくなる<br>
+            たまに食卓に新しい色をつけたくなる<br>
+            さあ、あなたの食卓を、本場中華の色へ</p>
+        </div>
+    </section>
+    <!--Mainvisualはここまで-->
+
+    <!--section #aboutはここから-->
+    <section id="access" class="my-24 pb-24 bg-gray-100">
+        <div class="md:flex justify-center">
+            <div class="">
+                <img class="w-30 rounded mx-auto pt-12" src="../img/author.jpg" alt="">
+            </div>
+            <div class="text-left lg:ml-8 px-8">
+                <p class="font-bold text-3xl pt-20 pb-5">食華とは？</p>
+                <p class="text-left mx-auto">什么是「食华」？</p>
+                <p class="mt-5 text-left mx-auto">
+                「食華」は、本場中華料理のレシピをかんたんに作成し、<br>
+                シェアできるサービスです。<br>
+                日本のスーパーで手軽に買える食材や調味料を手に入れ、<br>
+                日本にいる中華系の人でも、<br>
+                中華料理に興味を持っている人でも<br>
+                手軽に本場アレンジできます。</p>
+            </div>
+        </div>
+    </section>
+    <!--section #aboutはここまで-->
+    </main>
+
+    <!--Footerはここから-->
+    <footer>
+        <div class="bg-red-800">
+            <p class="text-neutral-50">お問い合わせ</p>
+            <p class="text-neutral-50 text-center">copyright<span>&copy;</span>2023 Francine Huang</p>
+        </div>
+    </footer>
+    <!--Footerはここまで-->
+</body>
+</html>
