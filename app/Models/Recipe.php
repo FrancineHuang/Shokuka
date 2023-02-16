d3581ed49545732f16a1bff61a25b2cd77ab404b
FrancineHuang <huangxin130@gmail.com>
2023-02-16 22:24:57 +0900

    レシピのモデル設定をしました。

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cover_photo_path',
+        'title',
+        'catchcopy',
+        'person',
+        'tip'
+    ];
 }
