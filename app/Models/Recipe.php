470a678becc5db3c90e40378d3453465a6d036e6
FrancineHuang <huangxin130@gmail.com>
2023-02-19 22:57:28 +0900

    レシピのモデルを紐つける部分を追加しました。

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\DB;
 
 class Recipe extends Model
 {
@@ -16,4 +19,31 @@ class Recipe extends Model
         'person',
         'tip'
     ];
+
+    //レシピを取得
+    public function getRecipe() {
+        $query_step = Request::query('step');
+        $query_ingredient = Request::query('ingredient');
+        $query_recipes = Recipe::query()
+            ->select('recipes.*')
+            ->where('user_id', '=', Auth::id())
+            ->whereNull('deleted_at')
+            ->get();
+        
+        if(!empty($query_step)) {
+            $query_recipes->leftJoin('recipe_steps', 'recipe_steps.recipe_id', '=', 'recipe.id')
+            ->where('recipe_steps.step_id', '=', $query_step);
+        }
+
+        if(!empty($query_ingredient)) {
+            $query_recipes->leftJoin('recipe_ingredients', 'recipe_ingredients.recipe_id', '=', 'recipe.id')
+            ->where('recipe_ingredients.ingredient_id', '=', $query_ingredient);
+        }
+
+        $recipes = $query_recipes->get();
+
+        return $recipes;
+
+    }
+
 }
