0020155a6f55800cdf3701645d45759f267eba2f
FrancineHuang <huangxin130@gmail.com>
2023-03-05 21:54:49 +0900

    Modelsの入力ミスを修正しました。

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -6,19 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Recipe extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'cover_photo_path',
+        //'cover_photo_path',
         'title',
         'introduction', //catchcopy -> introduction
         'person',
         'tip'
     ];
+
+
 
     //レシピを取得
     public function getRecipe() {
@@ -27,16 +29,15 @@ class Recipe extends Model
         $query_recipes = Recipe::query()
             ->select('recipes.*')
             ->where('user_id', '=', Auth::id())
-            ->whereNull('deleted_at')
-            ->get();
+            ->whereNull('deleted_at');
         
         if(!empty($query_step)) {
-            $query_recipes->leftJoin('recipe_steps', 'recipe_steps.recipe_id', '=', 'recipe.id')
+            $query_recipes->leftJoin('recipe_steps', 'recipe_steps.recipe_id', '=', 'recipes.id')
             ->where('recipe_steps.step_id', '=', $query_step);
         }
 
         if(!empty($query_ingredient)) {
-            $query_recipes->leftJoin('recipe_ingredients', 'recipe_ingredients.recipe_id', '=', 'recipe.id')
+            $query_recipes->leftJoin('recipe_ingredients', 'recipe_ingredients.recipe_id', '=', 'recipes.id')
             ->where('recipe_ingredients.ingredient_id', '=', $query_ingredient);
         }
 
@@ -45,5 +46,14 @@ class Recipe extends Model
         return $recipes;
 
     }
+
+    //ステップ写真があれば->保存する
+    //なければ->デフォルトの写真を使う。
+    //後ほどステップ写真の実装をするときにコメントアウトを外す。
+    /*protected function coverPhoto(): Attribute {
+        return Attribute::make(get: function($value) {
+            return $value ? '/storage/step_images/' . $value : '/default-step-photo.jpg';
+        });
+    }*/
 
 }
