74d99d3b802f94964b18de42d1c2fed74c6a35da
FrancineHuang <huangxin130@gmail.com>
2023-03-12 22:55:32 +0900

    belongsToの関係を定義しました

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ingredient extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'material',
+        'quantity'
+    ];
+
+    public function recipe() {
+        return $this->belongsTo(Recipe::class, 'recipe_id');
+    }
+
 }
--- app/Models/Step.php
+++ app/Models/Step.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Step extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'step_photo_path',
+        'content'
+    ];
+
+    public function recipe() {
+        return $this->belongsTo(Recipe::class, 'recipe_id');
+    }
+
 }
