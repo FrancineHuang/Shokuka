2db435e208f033d300b805e85af927237d7f9a47
FrancineHuang <huangxin130@gmail.com>
2023-03-20 06:30:49 +0900

    レシピの編集画面のルーティングを追加します

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,12 @@ Route::prefix('recipe')->group(function () {
     Route::get('/show/{recipe_id}', [RecipeController::class, 'showRecipe'])->name('recipe.show'); //レシピ投稿の表示ルート
     Route::get('/create', [RecipeController::class, 'createNewRecipe'])->name('recipe.create'); //レシピの作成ルート
     Route::post('/store', [RecipeController::class, 'storeNewRecipe'])->name('recipe.store'); //レシピの保存機能
-    Route::get('/update/{id}', [RecipeController::class, 'update'])->name('recipe.update'); //レシピの更新ルート
+    Route::get('/edit/{recipe_id}', [RecipeController::class, 'editRecipe'])->name('recipe.edit'); //レシピの更新ルート
     Route::get('/destroy/{id}', [RecipeController::class, 'destroy'])->name('recipe.destroy'); //レシピの削除機能
+});
+
+Route::get('/vue', function () {
+    return view('vuetest');
 });
 
 
