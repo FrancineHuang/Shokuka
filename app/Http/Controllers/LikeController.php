00400a64aed03960860c7b4389aa35a310baf2d9
FrancineHuang <huangxin130@gmail.com>
2023-09-20 13:22:18 +0900

    Fix the controller to show the follower number

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Recipe;
 use App\Models\Like;
+use App\Models\Follower;
 use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
@@ -29,5 +30,19 @@ class LikeController extends Controller
         $like->delete();
 
         return redirect()->back();
+    }
+
+    public function showFollowers() {
+        $user_id = Auth::id();
+        $showUserData = User::with('recipes')->find($user_id);
+        $showRecipeData = $showUserData->recipes;
+        $likedRecipes = $showUserData->likes()->with('recipe')->get();
+
+        // ユーザーがフォローしている人の数を取得
+        $followingCount = Follower::where('follower_id', $user_id)->count(); 
+        // そのユーザーをフォローしている人の数を取得
+        $followerCount = Follower::where('followee_id', $user_id)->count();
+    
+        return view('user.likes', compact('showUserData', 'showRecipeData', 'likedRecipes', 'followingCount', 'followerCount'));
     }
 }
--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -24,4 +24,18 @@ class MyPageController extends Controller
     
         return view('dashboard', compact('showUserData', 'showRecipeData', 'likedRecipes', 'followingCount', 'followerCount'));
     }
+
+    public function showFollowers() {
+        $user_id = Auth::id();
+        $showUserData = User::with('recipes')->find($user_id);
+        $showRecipeData = $showUserData->recipes;
+        $likedRecipes = $showUserData->likes()->with('recipe')->get();
+
+        // ユーザーがフォローしている人の数を取得
+        $followingCount = Follower::where('follower_id', $user_id)->count(); 
+        // そのユーザーをフォローしている人の数を取得
+        $followerCount = Follower::where('followee_id', $user_id)->count();
+    
+        return view('user.all_recipes', compact('showUserData', 'showRecipeData', 'likedRecipes', 'followingCount', 'followerCount'));
+    }
 }
